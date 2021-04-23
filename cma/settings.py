939fb8e38302943b962f2118f99bb402f67be4e1
Vicky <vickygogeta1@gmail.com>
2021-04-23 22:46:02 +0530

    to deploy to heroku

--- cma/settings.py
+++ cma/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
-
+import django_heroku
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -124,3 +124,5 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     BASE_DIR/"static",
 ]
+
+django_heroku.settings(locals())
